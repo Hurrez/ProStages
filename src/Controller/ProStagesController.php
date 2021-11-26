b080e21096d5ac39981b465ed821173e953d5ac3
ABDELHAK Yanis <yabdelhak@iutbayonne.univ-pau.fr>
2021-11-26 10:52:10 +0100

    Mise en place des 3 première page

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -13,8 +13,30 @@ class ProStagesController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('pro_stages/index.html.twig', [
-            'controller_name' => 'ProStagesController',
-        ]);
+        return new Response ('<html><body><h1> Bienvenue sur la page d\'accueil de Prostages </h1></body></html>');
+    }
+	
+	/**
+     * @Route("/entreprises", name="pro_stages_entreprises")
+     */
+    public function afficherEntreprises(): Response
+    {
+        return new Response ('<html><body><h1> Cette page affichera la liste des entreprises proposant un stage </h1></body></html>');
+    }
+	
+	/**
+     * @Route("/formations", name="pro_stages_formations")
+     */
+    public function afficherFormations(): Response
+    {
+        return new Response ('<html><body><h1> Cette page affichera la liste des formations de l\'IUT</h1></body></html>');
+    }
+	
+	/**
+     * @Route("/stage/{id}", name="pro_stages_stage")
+     */
+    public function afficherStage($id): Response
+    {
+        return new Response ('<html><body><h1> Cette page affichera le descriptif du stage ayant pour identifiant '.$id.'</h1></body></html>');
     }
 }
