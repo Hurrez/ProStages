ac1d710ab12183cad356300a11c9c350069968e1
ABDELHAK Yanis <yabdelhak@iutbayonne.univ-pau.fr>
2022-01-05 15:12:45 +0100

    Mise en place d'une boucle pour créer 15 Formations avec Faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,11 +10,19 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $dutInformatique = new Formation();
-        $dutInformatique->setNomCourt("DUTInfo");
-        $dutInformatique->setNomLong("Diplôme Universitaire Technologique Informatique");
-        $manager->persist($dutInformatique);
+        //Création d'un générateur de données faker
+        $faker = \Faker\Factory::create('fr_FR');
+        
+        $nbFormations=15;
 
+        for ($i=1;$i <= $nbFormations;$i++){
+            $dutInformatique = new Formation();
+            $dutInformatique->setNomCourt($faker->realText($maxNbChars=15,$indexSize=2));
+            $dutInformatique->setNomLong($faker->realText($maxNbChars=50,$indexSize=2));       
+            $manager->persist($dutInformatique);
+        }
+        
+        //envoyer en bd
         $manager->flush();
     }
 }
