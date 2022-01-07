437c73c5784e5bc92e6872ef2b0eb043243a6069
ABDELHAK Yanis <yabdelhak@iutbayonne.univ-pau.fr>
2022-01-07 16:52:27 +0100

    Création des différente page pour le tri par formation et ajout des route dans le controller

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -15,9 +15,21 @@ class ProStagesController extends AbstractController
      */
     public function afficherAccueil(): Response
     {
-		return $this->render('pro_stages/accueil.html.twig',['controller_name'=>'Accueil',]);
+        $repositoryStage=$this->getDoctrine()->getRepository(Stage::class);
+        $stages=$repositoryStage->findall();
+		return $this->render('pro_stages/accueil.html.twig',['controller_name'=>'Accueil','stages'=>$stages]);
     }
 	
+    /**
+     * @Route("/stage/{id}", name="pro_stages_stage")
+     */
+    public function afficherStage($id): Response
+    {
+        $repositoryStage=$this->getDoctrine()->getRepository(Stage::class);
+        $stage=$repositoryStage->find($id);
+		return $this->render('pro_stages/stages.html.twig',['stage'=>$stage,]);
+    }
+
 	/**
      * @Route("/entreprises", name="pro_stages_entreprises")
      */
@@ -31,33 +43,15 @@ class ProStagesController extends AbstractController
     /**
      * @Route("/entreprises/{id}", name="pro_stages_entreprises_trie")
      */
-    public function TriParEntreprises($id): Response
+    public function triParEntreprises($id): Response
     {
 		$repositoryEntreprise=$this->getDoctrine()->getRepository(Entreprise::class);
         $entreprise=$repositoryEntreprise->find($id);
         return $this->render('pro_stages/entreprises_trie.html.twig',['controller_name'=>'Trie par l\'entreprise : ','entreprise'=>$entreprise]);
     }
 	
-	/**
-     * @Route("/formations", name="pro_stages_formations")
-     */
-    public function afficherFormations(): Response
-    {
-		return $this->render('pro_stages/formations.html.twig',['controller_name'=>'Formations',]);
-    }
-	
-	/**
-     * @Route("/stage/{id}", name="pro_stages_stage")
-     */
-    public function afficherStage($id): Response
-    {
-        $repositoryStage=$this->getDoctrine()->getRepository(Stage::class);
-        $stage=$repositoryStage->find($id);
-		return $this->render('pro_stages/stages.html.twig',['stage'=>$stage,]);
-    }
-
     /**
-     * @Route("/entreprise/{idE}/stage/{idS}", name="pro_stages_stage_entreprise")
+     * @Route("/entreprise/{idE}/stage/{idS}", name="pro_stages_entreprise_stage")
      */
     public function afficherStageEntreprise($idE,$idS): Response
     {
@@ -65,4 +59,29 @@ class ProStagesController extends AbstractController
         $stage=$repositoryStage->find($idS);
 		return $this->render('pro_stages/stage_Entreprise.html.twig',['stage'=>$stage,]);
     }
+
+	/**
+     * @Route("/formations", name="pro_stages_formations")
+     */
+    public function afficherFormations(): Response
+    {
+		return $this->render('pro_stages/formations.html.twig',['controller_name'=>'Formations',]);
+    }
+
+    /**
+     * @Route("/formations/{id}", name="pro_stages_formations_trie")
+     */
+    public function triParFormations($id): Response
+    {
+		return $this->render('pro_stages/formations_trie.html.twig',['controller_name'=>'Formations',]);
+    }
+	
+	/**
+     * @Route("/formations/{idF}/stage/{idS}", name="pro_stages_formation_stage")
+     */
+    public function afficherStageFormation($idF,$idS): Response
+    {
+		return $this->render('pro_stages/stage_Formation.html.twig',['controller_name'=>'Formations',]);
+    }
+    
 }
