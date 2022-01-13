5b1ab406569713c9c49b117692daaf046001969e
ABDELHAK Yanis <yabdelhak@iutbayonne.univ-pau.fr>
2022-01-13 12:26:26 +0100

    Finalisation des commentaires

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -14,72 +14,98 @@ use App\Repository\FormationRepository;
 
 class ProStagesController extends AbstractController
 {
+    //Route de l'accueil
     /**
      * @Route("/", name="pro_stages_accueil")
      */
     public function afficherAccueil(StageRepository $repositoryStage): Response
     {
-        $stages=$repositoryStage->findall();
-		    return $this->render('pro_stages/accueil.html.twig',['controller_name'=>'page d\'accueil','stages'=>$stages]);
+      //Récupération de tous les stages
+      $stages=$repositoryStage->findall();
+
+      //Transmission à la vue des éléments à afficher
+		  return $this->render('pro_stages/accueil.html.twig',['controller_name'=>'page d\'accueil','stages'=>$stages]);
     }
 	
+    //Route permettant d'afficher les stages
     /**
      * @Route("/stage/{id}", name="pro_stages_stage")
      */
     public function afficherStage(Stage $stage,$id): Response
     {
-		    return $this->render('pro_stages/stages.html.twig',['controller_name'=>'page du stage : '.$id,'stage'=>$stage,]);
+      //Transmission à la vue des éléments à afficher
+		  return $this->render('pro_stages/stages.html.twig',['controller_name'=>'page du stage : '.$id,'stage'=>$stage,]);
     }
 
+    //Route affichant toutes les entreprises
 	  /**
      * @Route("/entreprises", name="pro_stages_entreprises")
      */
     public function afficherEntreprises(EntrepriseRepository $repositoryEntreprises): Response
     {
-        $entreprises=$repositoryEntreprises->findAll();
-        return $this->render('pro_stages/entreprises.html.twig',['controller_name'=>'page du tri par entreprise','entreprises'=>$entreprises]);
+      //Récupération de toute les entreprises
+      $entreprises=$repositoryEntreprises->findAll();
+
+      //Transmission à la vue des éléments à afficher
+      return $this->render('pro_stages/entreprises.html.twig',['controller_name'=>'page du tri par entreprise','entreprises'=>$entreprises]);
     }
 
+    //Route affichant les stages d'une entreprise en particulier
     /**
      * @Route("/entreprise/{id}", name="pro_stages_entreprises_trie")
      */
     public function triParEntreprises(Entreprise $entreprise,$id): Response
     {
-        return $this->render('pro_stages/entreprises_trie.html.twig',['controller_name'=>'stages de l\'entreprise : '.$id,'entreprise'=>$entreprise]);
+      //Transmission à la vue des éléments à afficher
+      return $this->render('pro_stages/entreprises_trie.html.twig',['controller_name'=>'stages de l\'entreprise : '.$id,'entreprise'=>$entreprise]);
     }
 	
+    //Route permettant d'afficher le stage d'une entreprise en particulier
     /**
      * @Route("/entreprise/{idE}/stage/{idS}", name="pro_stages_entreprise_stage")
      */
     public function afficherStageEntreprise(StageRepository $repositoryStage,$idE,$idS): Response
     {
-        $stage=$repositoryStage->find($idS);
-		    return $this->render('pro_stages/stage_Entreprise.html.twig',['controller_name'=>'page du stage : '.$idS,'stage'=>$stage,]);
+      //Récupération du stage possèdant l'id $idS
+      $stage=$repositoryStage->find($idS);
+
+      //Transmission à la vue des éléments à afficher
+		  return $this->render('pro_stages/stage_Entreprise.html.twig',['controller_name'=>'page du stage : '.$idS,'stage'=>$stage,]);
     }
 
+    //Route permettant d'afficher toutes les formations
   	/**
      * @Route("/formations", name="pro_stages_formations")
      */
     public function afficherFormations(FormationRepository $repositoryFormations): Response
     {
+      //Récupération de toute les formations
       $formations=$repositoryFormations->findAll();
+
+      //Transmission à la vue des éléments à afficher
 		  return $this->render('pro_stages/formations.html.twig',['controller_name'=>'page du tri par formation','formations'=>$formations]);
     }
 
+    //Route permettant d'afficher les stages d'une formation en particulier
     /**
      * @Route("/formation/{id}", name="pro_stages_formations_trie")
      */
     public function triParFormations(Formation $formation,$id): Response
     {
+      //Transmission à la vue des éléments à afficher
 	  	return $this->render('pro_stages/formations_trie.html.twig',['controller_name'=>'stages de la formations : '.$id,'formation'=>$formation]);
     }
 	
+    //Route permettant d'afficher le stages d'une formation en particulier
 	  /**
      * @Route("/formation/{idF}/stage/{idS}", name="pro_stages_formation_stage")
      */
     public function afficherStageFormation(StageRepository $repositoryStage,$idF,$idS): Response
     {
+      //Récupération du stages possèdant l'id $idS
       $stage=$repositoryStage->find($idS);
+      
+      //Transmission à la vue des éléments à afficher
 	  	return $this->render('pro_stages/stage_Formation.html.twig',['controller_name'=>'page du stage : '.$idS,'stage'=>$stage,]);
     }
     
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -17,42 +17,54 @@ class AppFixtures extends Fixture
        
         //Création formations
         $nombreFormations=5;
+        //Déclaration de tableaux permettant de définir des noms voulu aux formations
         $tableauFormationsLong = array("DUT Informatique","DUT Gestion Entreprise Administration","Licence Prog Avancée","BTS Electronique","Licence Pro Numérique");
         $tableauFormationsCourt = array("Info","GEA","Prog Avancée","Electronique","LP Num");
         for ($i=0;$i<$nombreFormations;$i++)
         {
-            $formation = new Formation();
+            $formation = new Formation(); //Création d'une nouvelle formation
             $formation->setNomCourt($tableauFormationsCourt[$i]);
             $formation->setNomLong($tableauFormationsLong[$i]);
-            $formations[]=$formation;    
-            $manager->persist($formation);
+
+            //Tableau contenant toutes les formations
+            $formations[]=$formation;   
+
+            $manager->persist($formation); //enregistrement en bd
         }
         
         //Création entreprises
         $nombreEntreprises=15;
         for ($i=0;$i<$nombreEntreprises;$i++)
         {
-            $entreprise = new Entreprise();
+            $entreprise = new Entreprise(); //Création d'une nouvelle entreprise
             $entreprise->setNom($faker->company);
             $entreprise->setAdresse($faker->address);
             $entreprise->setActivite($faker->realText($maxNbChars=35,$indexSize=2));
             $entreprise->setSiteweb($faker->url);
-            $entreprises[]=$entreprise;
-            $manager->persist($entreprise);
+
+            //Tableau contenant toutes les entreprises
+            $entreprises[]=$entreprise; 
+
+            $manager->persist($entreprise); //enregistrement en bd
         }
 
         //Création stages
         $nombreStages=25;
+        //Tableaux permettant de définir aléatoiremenbt 
         $tableauMetier = array("Développeur","Pentester","Stagiaire","Codeur","Algorithmicien","Déchet");
         $tableauLangage = array("C++","java","C","HTML","PHP","Symfony","Base de données","Reseau","Java script");
         for($i=0;$i<$nombreStages;$i++)
         {
-            $maxFormation = $faker->numberBetween($min=1,$max=3);
-            $stage = new Stage();
-            $stage->setTitre($tableauMetier[($faker->numberBetween($min=0,$max=count($tableauMetier)-1))]." en ".$tableauLangage[($faker->numberBetween($min=0,$max=count($tableauLangage)-1))]);
+            $maxFormation = $faker->numberBetween($min=1,$max=3); //Création d'un nombre aléatoire définissant le nombre de formations qu'aura le stage
+            $stage = new Stage(); //Création d'un nouveau stage
+            $stage->setTitre($tableauMetier[($faker->numberBetween($min=0,$max=count($tableauMetier)-1))]." en ".$tableauLangage[($faker->numberBetween($min=0,$max=count($tableauLangage)-1))]); //génération d'un titre avec les tableaux
             $stage->setMission($faker->realText($maxNbChars=150,$indexSize=2));
             $stage->setEmail($faker->email());
+
+            //Attribution d'une entreprise aléatoirement
             $stage->setEntreprise($entreprises[$faker->numberBetween($min=0,$max=count($entreprises)-1)]);
+
+            //Attribution des formations aléatoirement
             for($j=0;$j<$maxFormation;$j++)
             {
                 $formation = $faker->unique->numberBetween($min=0,$max=4);
@@ -60,10 +72,10 @@ class AppFixtures extends Fixture
             }
             $faker->unique($reset = true);
             
-            $manager->persist($stage);
+            $manager->persist($stage); //enregistrement en bd
         }
 
         //envoyer en bd
-        $manager->flush();
+        $manager->flush(); //Implementation
     }
 }
